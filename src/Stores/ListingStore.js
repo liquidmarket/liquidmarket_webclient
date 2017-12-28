f87185cfea609c80cf350decdbe9c7468d2de573
thomashorrobin <thomasroberthorrobin@gmail.com>
2017-12-29 11:58:04 +1300

    added listing wrapper

--- src/Stores/ListingStore.js
+++ src/Stores/ListingStore.js
@@ -12,7 +12,7 @@ class ListingStore extends EventEmitter {
     handleActions(action){
         switch (action.type) {
             case 'REFRESH_LISTING':
-                this.listings = action.listings;
+                this.listings = action.listings.map(l => new ListingWrapper(l));
                 break;
             default:
                 break;
@@ -26,3 +26,12 @@ const listingStore = new ListingStore();
 dispatcher.register(listingStore.handleActions.bind(listingStore));
 
 export default listingStore;
+
+class ListingWrapper {
+    constructor(listing){
+        Object.assign(this, listing);
+    }
+    get name(){
+        return `${ this.organisation_name } (${ this.short_name })`;
+    }
+}
